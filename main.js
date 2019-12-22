b6ad8de7355103931450a89aac2b6dae546cbd52
feelskill <maiersa@mail.ru>
2019-12-22 19:46:29 +0500

    сократил

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 "use strict"
 let titles = document.querySelectorAll('.accordion-title');
-let articleBody = document.querySelectorAll('.accordion-cont');
 
 for (let title of titles) {
     title.addEventListener('click', () => {
